fa23224d4eaf034dbd9dfba0cac1d1be64c99587
Tristan Petit <tristan.petit@etu.univ-grenoble-alpes.fr>
2025-03-13 06:25:51 +0100

    ajout script 	new file:   DOC-COM/document-rendus.aux 	new file:   DOC-COM/document-rendus.fdb_latexmk 	new file:   DOC-COM/document-rendus.fls 	new file:   DOC-COM/document-rendus.log 	new file:   DOC-COM/document-rendus.pdf 	new file:   DOC-COM/document-rendus.synctex.gz 	modified:   DOC-COM/document-rendus.tex 	new file:   DOC-COM/images/reseauxMain.png 	modified:   Livrable-1/Livrable1.fdb_latexmk 	modified:   Livrable-1/Livrable1.log 	modified:   Livrable-1/Livrable1.pdf 	modified:   Livrable-1/Livrable1.synctex.gz 	new file:   Prototype/file.aux 	new file:   Prototype/file.fdb_latexmk 	new file:   Prototype/file.fls 	new file:   Prototype/file.log 	new file:   Prototype/file.pdf 	new file:   Prototype/file.synctex.gz 	new file:   Prototype/file.tex 	new file:   Prototype/file.toc 	new file:   script/ping_test.sh

---
+++ script/ping_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#on teste la connectivité des machines entre elles
+
+#test si les paramètres donnés sont valides
+
+if [[ $# -lt 1 ]];
+  then 
+    echo "Il faut entrer des adresse IP valides pour tester la connexion sur le réseaux"
+    echo "$0 x.x.x.x y.y.y.y ou x et y sont des nombres"
+    exit 10
+  fi
+
+# Vérification des adresses IPv4
+regex_ip='^([0-9]{1,3}\.){3}[0-9]{1,3}$'
+
+for adresse in "$@"; 
+ do
+    if ! [[ $adresse =~ $regex_ip ]]; then
+        echo "Adresse IP invalide : $adresse"
+        exit 2
+    fi
+ done
+
+for ip_adress in "$@"; 
+  do
+
+    if ping -c 1 -w 1 "$ip_adress" &> /dev/null;
+      then
+        echo "$USER communique correctement avec $ip_adress"
+      else
+        echo "$USER n'arrive pas à communiquer avec $ip_adress"
+      fi
+
+  done
+
+
+
