ebc25876e18503b1f6e57e5861bbb541eea65f33
Tristan Petit <tristan.petit@etu.univ-grenoble-alpes.fr>
2025-03-13 19:59:51 +0100

    configuration routeur 	new file:   script/configure_routeur.sh 	modified:   script/testDNS.sh

--- script/testDNS.sh
+++ script/testDNS.sh
@@ -7,16 +7,23 @@ if [[ $# -lt 1 ]];
     exit 10
   fi
 
+# vérification d'un nom de domaine du type 
+
+for testHost in "$@";
+  do
+    if ! [[ $testHost =~ ^www.[a-z][a-z]*\.[a-z][a-z][a-z]*$ ]]; then 
+    echo "Domaine invalide : $testHost "
+      exit 1
+    fi
+  done
 
 for host in "$@";
   do 
-
-    if host "$host";
-      then
+    if host "$host" &> /dev/null; then
         echo "la résolution de nom de : $host fonctionne"
       else 
         echo "la résolution de nom de : $host a échoué"
-
+    fi
   done
 
 
