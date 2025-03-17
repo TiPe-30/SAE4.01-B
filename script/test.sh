c8f7eddce8cb069ecf4a93b1212d2f37068ee9ea
Tristan Petit <tristan.petit@etu.univ-grenoble-alpes.fr>
2025-03-17 14:58:46 +0100

    new file:   Prototype/DocTechnique/resVirt/resVirtuel.tex 	new file:   Prototype/DocTechnique/routeurs/routeurs.tex 	new file:   Prototype/Images/Logo-project.jpeg 	new file:   Prototype/Sec 	modified:   Prototype/file.aux 	modified:   Prototype/file.fdb_latexmk 	modified:   Prototype/file.fls 	modified:   Prototype/file.log 	modified:   Prototype/file.pdf 	modified:   Prototype/file.synctex.gz 	modified:   Prototype/file.tex 	modified:   Prototype/file.toc 	modified:   script/install_dhcp.sh 	modified:   script/test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,19 +1,61 @@
 #!/bin/bash
 
-cat <<FILE 
-#!/usr/sbin/nft -f
+read -r -p "Entrez l'interface que vous souhaitez configurer : " interface
 
-flush ruleset
+read -r -p "Entrez l'adresse du réseau en notation CIDR(ex : 192.168.13.24/24) : " reseau
 
-table ip filter {
-     chain input {
-          type filter hook input priority 0; policy drop;
+read -r -p "Entrez l'adresse Ip de votre routeur : " routeur
 
-          # accept traffic originated from us
-          ct state established,related accept
+echo "Entrez la plage d'adresse que vous souhaitez (tapez l'adresse de début + l'adresse de fin) à la suite : "
+read -r addrDebut 
+read -r addrFin
 
-          # accept any localhost traffic
-          iif lo accept
-     }
+read -r -p "Entrez l'adresse ip du serveur DNS : " dns
+
+cat <<DHCP
+
+{
+    "Dhcp4": {
+        "interfaces-config": {
+            "interfaces": [
+                "$interface"
+            ]
+        },
+        "valid-lifetime": 691200,
+        "renew-timer": 345600,
+        "rebind-timer": 604800,
+        "authoritative": true,
+        "lease-database": {
+            "type": "memfile",
+            "persist": true,
+            "name": "/var/lib/kea/kea-leases4.csv",
+            "lfc-interval": 3600
+        },
+        "subnet4": [
+            {
+                "subnet": "$reseau",
+                "pools": [
+                    {
+                        "pool": "$addrDebut - $addrFin"
+                    }
+                ],
+                "option-data": [
+                    {
+                        "name": "domain-name-servers",
+                        "data": "$dns"
+                    },
+                    {
+                        "name": "domain-search",
+                        "data": "it-connect.local"
+                    },
+                    {
+                        "name": "routers",
+                        "data": "$routeur"
+                    }
+                ]
+            }
+        ]
+    }
 }
-FILE
+
+DHCP
