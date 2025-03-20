907e9e147d6253fe8fe2a62d07e5b7dd0cf7273a
elsebeim <elsebeim@etu.univ-grenoble-alpes.fr>
2025-03-20 08:09:32 +0100

    Amelioration Script Add User Powershell

--- script/Automatisation/Add-User-Adv.ps1
+++ script/Automatisation/Add-User-Adv.ps1
@@ -1,24 +1,22 @@
 ###################CONSTANTE##################################
-$OU = "CN=Users,DC=LOCAL,DC=PLETER,DC=OVH"
-$DomainName = "LOCAL.PLETER.OVH"
+$OU = "CN=Users,DC=LOCAL,DC=PLETER,DC=OVH"  #Organizational Units où insérer l'utilisateur
+$DomainName = "LOCAL.PLETER.OVH" #Domaine Active Directory
 
 ###################MAIN#######################################
 
-#
-$FirstName = Read-Host "Entrez le prénom de l'utilisateur "
-$LastName = Read-Host "Entrez le nom de famille de l'utilisateur "
+#Demander paramètres utilisateur
+$FirstName = Read-Host "Entrez le prénom du nouvel utilisateur "
+$LastName = Read-Host "Entrez le nom de famille du nouvel utilisateur "
 $DefaultSAM = "$($FirstName.Substring(0,1))$LastName".ToLower()
 #Demander sAMAccountName du nouvel utilisateur
-$SAMAccountName = Read-Host "sAMAccountName [$DefaultSAM)] "
+$SAMAccountName = Read-Host "sAMAccountName du nouvel utilisateur [$DefaultSAM)] "
 if (-not $SAMAccountName) { $SAMAccountName = $DefaultSAM }
 
 
 $Password = Read-Host "Password" -AsSecureString
 
 
-
-
-#Mise en forme du nouvel utilisateur
+#Mise en forme des paramètres du nouvel utilisateur
 $NewADUserParameters = @{
   Name = "$FirstName $LastName"
   GivenName = $FirstName
